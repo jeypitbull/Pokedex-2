72fd070abe9d51232ab19648ee3136b6da11151f
Jeisson Gomez <jeypitbullz4@gmail.com>
2020-10-05 17:18:52 -0500

    Creacion de pokemon list

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -6,7 +6,12 @@ export default class NavBar extends Component {
         return (
             <div>
                 <nav className = "navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                    <a>Pokedex</a>
+                    <a
+                        href = ""
+                        className = "navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
+                    >
+                        Pokedex
+                    </a>
                 </nav>
             </div>
         )
