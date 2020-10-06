dbf4510cb385b49242f5b733d9d00256383e6a07
Jeisson Gomez <jeypitbullz4@gmail.com>
2020-10-06 16:18:23 -0500

    Sombras a las imagenes

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import spinner from '../pokemon/spinner.gif';
 
 const Sprite = styled.img`
     width: 5em;
     height: 5em;
+    display: none;
+`;
+
+const card = styled.div`
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `;
 
 export default class PokemonCard extends Component {
@@ -35,13 +41,32 @@ componentDidMount(){
                         {this.state.pokemonIndex}
                     </h5>
 
+                    {this.state.imageLoading ? (
+                        <img
+                            src = {spinner}
+                            style ={{width: '5em', height: '5em'}}
+                            className = "card-img-top rounded mx-auto d-block mt-2"
+                        />
+                    ) : null}
+
                     <Sprite
                         className = "card-img-top rounded mx-auto mt-2"
                         onLoad = {() => this.setState({imageLoading: false})}
                         onError = {() => this.setState({toManyRequests: true})}
                         src = {this.state.imageUrl}
-                        
+                        style = {
+                            this.state.toManyRequests ? {display: "none"}:
+                            this.state.imageLoading ? null : {display: "block"}
+                        }
                     />
+
+                    {this.state.toManyRequests ? (
+                        <h6 className = "mx-auto">
+                            <span className = "badge badge-danger mt-2">
+                                To Many Request
+                            </span>
+                        </h6>
+                    ) :null}
 
                     <div className = "card-body mx-auto">
                         <h6 className = "card-title">
