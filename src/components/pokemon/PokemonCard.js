f8166c613a531478e98875fa10375fc11a5ad23a
Jeisson Gomez <jeypitbullz4@gmail.com>
2020-10-06 13:40:45 -0500

    Nombre del pokemon

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,11 +8,14 @@ state = {
 };
 
     render() {
+        const name = this.props.name;
+        const url = this.props.url;
+
         return (
             <div className = 'col-md-3 col-sm-6 mb-5'>
                 <div className = "card">
                     <div className = "card-header">
-                        <h1 />
+                        <h1>{name}</h1>
                     </div>
                 </div>
             </div>
